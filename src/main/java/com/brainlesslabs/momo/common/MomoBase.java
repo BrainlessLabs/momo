78cf783ff85e384b33b08f08220ba798ed9c11f2
Shakti Misra <shakti.misra@sap.com>
2019-10-12 17:04:48 +0530

    Adding different policy

--- src/main/java/com/brainlesslabs/momo/common/MomoBase.java
+++ src/main/java/com/brainlesslabs/momo/common/MomoBase.java
@@ -4,27 +4,80 @@ import com.brainlesslabs.momo.common.exceptions.MomoException;
 import com.brainlesslabs.momo.common.options.Options;
 import com.brainlesslabs.momo.common.options.ReadOptions;
 import com.brainlesslabs.momo.common.options.WriteOptions;
+import com.brainlesslabs.momo.common.policies.CompressionPolicy;
+import com.brainlesslabs.momo.common.policies.StoragePolicy;
+import com.brainlesslabs.momo.common.utils.ByteSlice;
 import com.brainlesslabs.momo.common.utils.DbStatus;
 import com.brainlesslabs.momo.common.utils.MomoIterator;
 import com.brainlesslabs.momo.common.utils.Snapshot;
+import lombok.AllArgsConstructor;
 import lombok.NonNull;
 
-public interface MomoBase {
-    void open(@NonNull final Options options, Object... params) throws MomoException;
+@AllArgsConstructor
+public abstract class MomoBase {
+    private StoragePolicy storagePolicy;
+    private Options options;
+    private CompressionPolicy compressionPolicy;
 
-    void put(@NonNull final WriteOptions writeOptions, @NonNull final byte[] key, @NonNull final byte[] value) throws MomoException;
+    void open(Object... params) throws MomoException {
+        storagePolicy.open(options, params);
+    }
 
-    byte[] get(@NonNull final ReadOptions readOptions, @NonNull final byte[] key) throws MomoException;
+    void put(@NonNull final WriteOptions writeOptions,
+             @NonNull final byte[] key,
+             @NonNull final byte[] value) throws MomoException {
+        ByteSlice keySlice = new ByteSlice(key, key.length);
+        ByteSlice valueSlice = new ByteSlice(value, value.length);
+        if (isCompressionEnabled()) {
+            valueSlice = compressionPolicy.compress(valueSlice);
+        }
+        storagePolicy.put(writeOptions, keySlice, valueSlice);
+    }
 
-    void delete(@NonNull final WriteOptions writeOptions, @NonNull final byte[] key) throws MomoException;
+    byte[] get(@NonNull final ReadOptions readOptions, @NonNull final byte[] key) throws MomoException {
+        ByteSlice keySlice = new ByteSlice(key, key.length);
+        ByteSlice valueSlice = storagePolicy.get(readOptions, keySlice);
+        if(isCompressionEnabled()) {
+            valueSlice = compressionPolicy.decompress(valueSlice);
+        }
+        return valueSlice.getBytes();
+    }
 
-    MomoIterator getIterator() throws MomoException;
+    void delete(@NonNull final WriteOptions writeOptions, @NonNull final byte[] key) throws MomoException {
+        storagePolicy.delete(writeOptions, key);
+    }
 
-    DbStatus getDbStatus() throws MomoException;
+    MomoIterator getIterator() throws MomoException {
+        return storagePolicy.getIterator();
+    }
 
-    void close() throws MomoException;
+    DbStatus getDbStatus() throws MomoException {
+        return null;
+    }
 
-    Snapshot snapshot() throws MomoException;
+    void close() throws MomoException {
+        storagePolicy.close();
+    }
 
-    void destroyDb() throws MomoException;
+    Snapshot snapshot() throws MomoException {
+        return storagePolicy.snapshot();
+    }
+
+    void destroyDb() throws MomoException {
+        storagePolicy.destroyDb();
+    }
+
+    private boolean isCompressionEnabled() {
+        boolean result;
+        switch (options.getCompressionMode()) {
+            case DEFAULT:
+            case DO_NOT_COMPRESS:
+                result  = false;
+                break;
+            case COMPRESS:
+            default:
+                result = true;
+        }
+        return result;
+    }
 }
